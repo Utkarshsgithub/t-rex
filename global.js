a84cb7d3afb9e6149ef4c4945253bb214bb433fc
Utkarshsgithub <utkarshcodes13@gmail.com>
2022-02-28 19:24:58 +0530

    form and footer made responsive

--- global.js
+++ global.js
@@ -4,3 +4,9 @@ if (winWidth<1060) {
     let stageTwo = document.getElementById('stage-2');
     stageTwo.style.flexDirection = 'column';
 }
+
+let registerText = document.getElementById('register-text');
+
+if (winWidth<550) {
+    registerText.innerHTML = 'Register To Get Rewards'
+}
